3304ab3ef3f4500be8ee2720ddedaf330a9b626b
Iuri Guilherme <2027447+iuriguilherme@users.noreply.github.com>
2022-01-12 11:07:22 -0300

    HV Cursos

--- plugins/welcome.py
+++ plugins/welcome.py
@@ -29,6 +29,11 @@ from plugins.personalidades import (
     gerar_texto,
 )
 
+indesejaveis = [
+    'HV Cursos',
+    'MateBot (dev)',
+]
+
 async def add_handlers(dispatcher):
     ## Padrão de boas vindas. Exclui grupos 'omega' pra evitar de mandar mensagem
     ## de boas vindas em grupos onde o bot só é utilizado com os comandos básicos.
@@ -44,5 +49,10 @@ async def add_handlers(dispatcher):
     async def welcome_callback(message: types.Message):
         await message_callback(message, ['welcome', message.chat.type])
         text = await gerar_texto('welcome', dispatcher.bot, message)
+        if message['new_chat_member']['first_name'] in indesejaveis:
+            text = u"""sem querer caguetar @admin, mas taí de novo o {}\
+""".format(
+                message['new_chat_member']['first_name'],
+            )
         command = await message.reply(text)
         await command_callback(command, ['welcome', message.chat.type])
