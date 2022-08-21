5881cc794022a46e4e5e4c3ed161730fba2e89b9
Iuri Guilherme <2027447+iuriguilherme@users.noreply.github.com>
2022-08-21 17:27:45 -0300

    Corrigindo erros e documentação

--- plugins/tc.py
+++ plugins/tc.py
@@ -1,5 +1,6 @@
 # vim:fileencoding=utf-8
 """Plugin tc para ia.cecil"""
+
 #  Copyleft (C) 2022 Iuri Guilherme <https://iuri.neocities.org/>
 #  
 #  This program is free software: you can redistribute it and/or modify
@@ -26,32 +27,42 @@ from iacecil.controllers.aiogram_bot.callbacks import (
     message_callback,
 )
 
-def dice(faces = 6, *args, **kwargs) -> int:
-    return numpy.random.randint(1, facets + 1)
+def dice(faces = 6, seed = None, *args, **kwargs) -> int:
+    numpy.random.seed(seed)
+    return numpy.random.randint(1, faces + 1)
 
 def coord(seed = None, *args, **kwargs) -> float:
-    if seed is not None:
-        numpy.random.seed(seed)
+    numpy.random.seed(seed)
     return numpy.random.rand()
 
-## Aiogram
 async def add_handlers(dispatcher):
+    """
+    Registra handlers em um Aiogram Dispatcher caso este plugin esteja
+    habilitado.
+    """
     try:
         ## Rola um dado
         @dispatcher.message_handler(
             commands = ['dice', 'dado', 'd'],
         )
         async def dice_callback(message):
+            """
+            Retorna um valor pseudo aleatório de uma face de um dado com 
+            tantos lados quantos o primeiro argumento. Padrão: 6
+            """
             await message_callback(message, ['tc', 'dice', message.chat.type])
             faces = 6
             try:
-                faces = int(message.get_args())
-                if not faces > 0:
-                    raise ValueError()
+                if message.get_args() not in [None, '', ' ', 0]:
+                    faces = int(message.get_args())
+                    if not faces > 0:
+                        raise ValueError()
             except ValueError:
-                await message.reply(f"""{message.get_args()} não é um número \
-de faces de um dado válido, vou usar um dado de seis faces normal.""")
-            command = await message.reply(dado(faces))
+                pass
+                # ~ await message.reply(f"""{message.get_args()} não é um \
+# ~ número de faces de um dado válido, vou usar um dado de seis faces normal.\
+# ~ """)
+            command = await message.reply(dice(faces))
             await command_callback(command, ['tc', 'dice', message.chat.type])
     except Exception as exception:
         raise
