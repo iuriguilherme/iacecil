38398442161dbcb79137be94d7eafb0ef00947a7
Iuri Guilherme <2027447+iuriguilherme@users.noreply.github.com>
2022-08-22 17:17:00 -0300

    Gráfico com estatísticas

--- plugins/tc.py
+++ plugins/tc.py
@@ -21,7 +21,9 @@ import logging
 logger = logging.getLogger(__name__)
 
 import numpy
-from aiogram import Dispatcher
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
+from matplotlib import pyplot
+from io import BytesIO
 from iacecil.controllers.aiogram_bot.callbacks import (
     command_callback,
     error_callback,
@@ -35,29 +37,32 @@ from iacecil.controllers.persistence.zodb_orm import (
 from plugins.mate_matica import dice
 
 ## Valores padrão
-faces = 3
+faces = 6
 comando = '/torre'
 
-async def level_down(bot_id: str, user_id: str, levels: list) -> int:
+async def level_down(dispatcher.bot.id: str, user_id: str, levels: list) -> int:
     level = max(0, levels[-1] - 1)
-    await set_tc_level(bot_id, user_id, level)
+    await set_tc_level(dispatcher.bot.id, user_id, level)
     return (level, 0)
 
-async def level_up(bot_id: str, user_id: str, levels: list) -> int:
+async def level_up(dispatcher.bot.id: str, user_id: str, levels: list) -> int:
     level = levels[-1] + 1
-    await set_tc_level(bot_id, user_id, level)
+    await set_tc_level(dispatcher.bot.id, user_id, level)
     return (level, 0)
 
-async def levels_up(bot_id: str, user_id: str, levels: list) -> int:
+async def levels_up(dispatcher.bot.id: str, user_id: str, levels: list) -> int:
     roll = dice(faces)
     level = levels[-1] + roll
-    await set_tc_level(bot_id, user_id, level)
+    await set_tc_level(dispatcher.bot.id, user_id, level)
     return (level, roll)
 
 mapa = {
     1: level_down,
-    2: level_up,
-    3: levels_up,
+    2: level_down,
+    3: level_down,
+    4: level_up,
+    5: level_up,
+    6: levels_up,
 }
 
 async def add_handlers(dispatcher):
@@ -77,13 +82,19 @@ async def add_handlers(dispatcher):
             command = await message.reply(f"""Instruções: Cada jogador começa \
 no térreo de uma torre de andares infinitos. Use o comando /rolar ou /roll \
 para rolar um dado de {faces} faces. Caso o resultado seja:\n\n\
-1: Desça um andar;\n\
-2: Suba um andar;\n\
-3: Role o dado novamente e suba tantos andares quanto o valor do segundo \
+1, 2 ou 3: Desça um andar;\n\
+4 ou 5: Suba um andar;\n\
+6: Role o dado novamente e suba tantos andares quanto o valor do segundo \
 dado.\n\n\
-Não é possível descer além do térreo (andar 0). Para ver as estatísticas do \
-jogo, espere este comando existir. Para apagar todas as estatísticas, espere \
-este comando existir.""")
+Não tem como ganhar o jogo nem perder, não é possível descer além do térreo \
+(andar 0). O jogo dura enquanto eu pagar a hospedagem do servidor.\n\
+Para ver as estatísticas individuais, use o comando /aonde ou /where\n\
+Para ver as estatísticas globais (ranking), espere este comando existir.\n\
+Para apagar todas as estatísticas, espere este comando existir.\n\
+Para doar dinheiro e ajudar a manter o jogo no ar (e provavelmente adicionar \
+mais elementos de jogo), fale com o desenvolvedor.\n\
+Quem quiser começar no andar três mil pode propor relações mutuamente \
+benéficas envolvendo favores sexuais para o desenvolvedor.""")
             await command_callback(command, ['tc', 'torre', message.chat.type])
 
         @dispatcher.message_handler(
@@ -96,24 +107,69 @@ este comando existir.""")
             """
             await message_callback(message, ['tc', 'roll', message.chat.type])
             try:
-                bot_id = Dispatcher.get_current().bot.id
                 roll = dice(faces)
-                rolled = await set_tc_roll(bot_id, message.from_id, roll)
-                levels = [v[1] for v in await get_tc_levels(bot_id,
+                rolled = await set_tc_roll(dispatcher.bot.id, message.from_id, roll)
+                levels = [v[1] for v in await get_tc_levels(dispatcher.bot.id,
                     message.from_id)]
-                level, new_roll = await mapa[roll](bot_id, message.from_id,
+                level, new_roll = await mapa[roll](dispatcher.bot.id, message.from_id,
                     levels)
                 if new_roll > 0:
                     command = await message.reply(f"""Resultado: {str(roll)}! \
-Segundo dado: {str(new_roll)}. Novo nível: {str(level)}. Para instruções, \
-clique em {comando}""")
+Segundo dado: {str(new_roll)}. Novo nível: {str(level)}.\nPara jogar de novo, \
+clique em /rolar\nPara ver as estatísticas, clique em /aonde\nPara \
+instruções, clique em {comando}""")
                 else:
                     command = await message.reply(f"""Resultado: {str(roll)}. \
-Novo nível: {str(level)}. Para instruções, clique em {comando}""")
+Novo nível: {str(level)}.\nPara jogar de novo, clique em /rolar\nPara ver as \
+estatísticas, clique em /aonde\nPara instruções, clique em {comando}""")
             except Exception as exception:
                 logger.exception(exception)
                 command = await message.reply("""Problemas técnicos. Avise o \
 desenvolvedor (se é que já não avisaram) e tente novamente mais tarde.""")
             await command_callback(command, ['tc', 'roll', message.chat.type])
+
+        @dispatcher.message_handler(
+            commands = ['where', 'aonde'],
+        )
+        async def where_callback(message):
+            """
+            Exibe um gráfico com o histórico de níveis deste user_id
+            """
+            await message_callback(message, ['tc', 'where', message.chat.type])
+            figure_buffer = BytesIO()
+            try:
+                levels = [v[1] for v in await get_tc_levels(dispatcher.bot.id,
+                    message.from_id)]
+                pyplot.plot(levels)
+                pyplot.title(f"""nível a cada jogada de \
+{message['from']['first_name']}""")
+                pyplot.xlabel("número da jogada")
+                pyplot.ylabel("andar da torre")
+                pyplot.xaxis.get_major_locator().set_params(integer=True)
+                pyplot.yaxis.get_major_locator().set_params(integer=True)
+                pyplot.savefig(figure_buffer, format = "png")
+                try:
+                    command = await message.reply_photo(
+                        figure_buffer.getbuffer(),
+                        caption = f"""Para jogar de novo, clique em /rolar\n\
+Para ver as estatísticas, clique em /aonde\nPara instruções, clique em \
+{comando}""",
+                    )
+                except Exception as exception:
+                    await erro_callback(
+                        u"Error trying to send graphic",
+                        message,
+                        exception,
+                        ['tc', 'where', 'exception'],
+                    )
+                    raise
+            except Exception as exception:
+                logger.exception(exception)
+                command = await message.reply("""Problemas técnicos. Avise o \
+desenvolvedor (se é que já não avisaram) e tente novamente mais tarde.""")
+            finally:
+                figure_buffer.close()
+            await command_callback(command, ['tc', 'where', message.chat.type])
     except Exception as exception:
+        logger.exception(exception)
         raise
