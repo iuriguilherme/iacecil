60b9a236c45d6e2e932363ff12594738f58b6ce0
Iuri Guilherme <2027447+iuriguilherme@users.noreply.github.com>
2023-02-23 01:35:16 -0300

    A mistake that costed a CPU core

--- src/plugins/openai.py
+++ src/plugins/openai.py
@@ -132,7 +132,7 @@ async def add_handlers(dispatcher: Dispatcher) -> None:
         async def chance_gpt_callback(message: types.Message) -> None:
             """Responde toda e qualquer mensagem em uma chance \
 aleatória"""
-            if len(message.get_args()) > 15 and await dice_low(30):
+            if len(message.text) > 15 and await dice_low(30):
                 await chatgpt_callback(message)
     except Exception as e:
         logger.exception(e)
